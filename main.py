7657cace0fb080c89e5ccc55e368a68b63f9c131
Jhon19011 <jhorivaco@gmail.com>
2025-02-28 11:46:19 -0500

    Nueva version

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import plotly.express as px
 
 st.title("Visualización de ejemplo")
 
-st.subtitle("Subtitulo")
+st.subheader("Subtitulo")
 
-data = (
-    "Categoría"=["A","B","C","D","E"],
-    "Valores"=[10,20,30,40,15]
-)
+data={
+    "Categoría":["A","B","C","D","E"],
+    "Valores":[10,20,30,40,15]
+}
 
 df = pd.DataFrame(data)
 
@@ -18,8 +18,8 @@ st.dataframe(df) #Muestro tabla
 
 st.subheader("Grafico sencillo entre lineas")
 
-fig, ax = plt.subplot()
-ax.plot(["Categoría"],df["Valores"],marker="o", linestyle="-",color="b")
+fig, ax = plt.subplots()
+ax.plot(df["Categoría"],df["Valores"],marker="o", linestyle="-",color="b")
 ax.set_xlabel("Categoría")
 ax.set_ylabel("Valor")
 ax.set_title("Tenddencia Valores")
@@ -28,5 +28,5 @@ st.pyplot(fig) #Se muestra en streamlit
 
 st.subheader("Grafico de barras")
 
-fig_bar = px.bar(df, x="Categoría", y ="Valores",text="Valores", color = "Categoria",title="")
+fig_bar = px.bar(df, x="Categoría", y ="Valores",text="Valores", color = "Categoría",title="Distribución de valores")
 st.plotly_chart(fig_bar)
